be7804a1e5b34b0e58030e893b778c993e994d25
Tim Van Baak <tim.vanbaak@gmail.com>
2019-07-09 12:26:13 -0700

    Sort items by time

--- inquisitor/app.py
+++ inquisitor/app.py
@@ -71,6 +71,9 @@ def root():
 				total += 1
 				if not any(map(lambda f: f(item), filters)):
 					active_items.append(item)
+	# Sort items by time
+	active_items.sort(key=lambda i: i['time'] if 'time' in i else 0)
+
 	logger.info("Returning {} of {} items".format(len(active_items), total))
 	if item_read_exceptions:
 		read_ex = {
