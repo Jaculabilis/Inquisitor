c3a83a80cc9a67d1d6a537c6231c4830af9154db
Tim Van Baak <tim.vanbaak@gmail.com>
2020-08-06 13:33:11 -0700

    Add requirements.txt

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 import setuptools
 
+with open("requirements.txt") as f:
+	requires = f.readlines()
+
 setuptools.setup(
 	name="inquisitor",
 	version="0.0.1",
@@ -7,4 +10,5 @@ setuptools.setup(
 	description="An arbitrary feed aggregator",
 	packages=setuptools.find_packages(),
 	python_requires=">=3.6",
+	install_requires=requires,
 )
